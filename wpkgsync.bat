d4e0ea65cafca7d904472e056d2683f7489120b9
Dave Evans <dave.evans@goodness.co.uk>
2020-04-29 16:19:49 +0100

    Update wpkgsync.bat
    if condition (
        do something
    ) else (
        do something else
    )
    doesn't always work reliably, so don't be clever.

--- wpkgsync.bat
+++ wpkgsync.bat
@@ -84,7 +84,7 @@ rem rsync variables in linux format, colon defines a remote host, so:
 rem replace c: with /cygdrive/c and \ with /  Example: C:\WORK\* --> /cygdrive/c/work/*
 set wpkgidfile=/cygdrive/c/ProgramData/wpkgsync/.ssh/wpkgsyncuser.id
 set wpkg_hosts=/cygdrive/c/ProgramData/wpkgsync/.ssh/known_hosts
-set wpkglocal=/cygdrive/c/ProgramData/wpkg/
+set wpkglocal=/cygdrive/c/ProgramData/wpkg
 
 rem rsync/ssh options
 rem  -r                     recurse into folders
@@ -128,64 +128,62 @@ rem can't talk to the remote server will result in code 12
 rem can't find the file/folder you're after will result in code 23
 
 :SETUP
-if '%setup%'=='True' (
-	rem just the top level, no packages as yet, no logging, because we'll be running this whilst wpkg is active
-	set exclude=--exclude='profiles.sites' --exclude='packages/'
-	rsync -r -e "ssh -i %wpkgidfile% -o UserKnownHostsFile=%wpkg_hosts%" --partial --times --timeout=120 %bwlimit% %exclude% --log-file=%wpkglogfile% %wpkgremote%:/opt/updates/ %wpkglocal%/ > nul 2>&1
-	set sync_setup=%errorlevel%
-	rem if it failed, then abort, so that automated install will abort
-	if not %sync_setup%==0 exit /b %sync_setup%
-	echo error=%sync_setup%
-	goto RSYNC-SEND
+if '%setup%'=='' goto RSYNC-GET-EXCLUDE
+rem just the top level, no packages as yet, no logging, because we'll be running this whilst wpkg is active
+set exclude=--exclude='profiles.sites' --exclude='packages/'
+rsync -r -e "ssh -i %wpkgidfile% -o UserKnownHostsFile=%wpkg_hosts%" --partial --times --timeout=120 %bwlimit% %exclude% %wpkgremote%:/opt/updates/ %wpkglocal%/ >nul 2>&1
+set sync_setup=%errorlevel%
+rem if it failed, then abort, so that automated install will abort
+if not %sync_setup%==0 exit /b %sync_setup%
+rem and skip all the next bits
+goto FIX_ACLS
 
-) else (
+:RSYNC-GET-EXCLUDE
+rem try to get any exclude file, sync this folder first regardless of whether it exists on the remote server
+rsync -rvh -e "ssh -i %wpkgidfile% -o UserKnownHostsFile=%wpkg_hosts%" --progress --times --timeout=120 %bwlimit% --log-file=%wpkglogfile% %wpkgremote%:/opt/updates/packages/exclude %wpkglocal%/packages/ 2>nul
+rem if that failed for any reason, just move on
 
-	:RSYNC-GET-EXCLUDE
-	rem try to get any exclude file, sync this folder first regardless of whether it exists on the remote server
-	rsync -rvh -e "ssh -i %wpkgidfile% -o UserKnownHostsFile=%wpkg_hosts%" --progress --times --timeout=120 %bwlimit% --log-file=%wpkglogfile% %wpkgremote%:/opt/updates/packages/exclude %wpkglocal%/packages/ 2>nul
-	rem if that failed for any reason, just move on
+:RSYNC-GET
+rem if there's a specific exclude file for a machine then use it, exclude-from works with DOS syntax too
+set exclude=--exclude='profiles.sites' --exclude='client/'
+if exist %packages%\exclude\pack_excl.default.txt set exclude=--exclude-from=%packages%\exclude\pack_excl.default.txt
+if exist %packages%\exclude\pack_excl.%computername%.txt set exclude=--exclude-from=%packages%\exclude\pack_excl.%computername%.txt
 
-	:RSYNC-GET
-	rem if there's a specific exclude file for a machine then use it, exclude-from works with DOS syntax too
-	set exclude=--exclude='profiles.sites' --exclude='client/'
-	if exist %packages%\exclude\pack_excl.default.txt set exclude=--exclude-from=%packages%\exclude\pack_excl.default.txt
-	if exist %packages%\exclude\pack_excl.%computername%.txt set exclude=--exclude-from=%packages%\exclude\pack_excl.%computername%.txt
+rem sync the remote structure to here, append the log to the wpkg log (wpkg creates a new one each time):
+rsync -rvh -e "ssh -i %wpkgidfile% -o UserKnownHostsFile=%wpkg_hosts%" --progress --delete --delete-excluded --partial --times --timeout=120 %bwlimit% %exclude% --log-file=%wpkglogfile% %wpkgremote%:/opt/updates/ %wpkglocal%/ 2>nul
+set sync_get=%errorlevel%
 
-	rem sync the remote structure to here, append the log to the wpkg log (wpkg creates a new one each time):
-	rsync -rvh -e "ssh -i %wpkgidfile% -o UserKnownHostsFile=%wpkg_hosts%" --progress --delete --delete-excluded --partial --times --timeout=120 %bwlimit% %exclude% --log-file=%wpkglogfile% %wpkgremote%:/opt/updates/ %wpkglocal%/ 2>nul
-	set sync_get=%errorlevel%
-	echo error=%sync_get%
-
-	:RSYNC-SEND
-	rem get last logged off user from eventlog, and append to the logfile
-	if exist %SCRIPTS%\wpkguserdetails.vbs cscript //nologo %SCRIPTS%\wpkguserdetails.vbs >> "%wpkglogfile%"
-	rem set wpkglogfile for rsync, add /cygdrive/ and remove colons
-	set wpkglogfile=/cygdrive/%wpkglogfile::=%
-	rem replace \ with /
-	set wpkglogfile=%wpkglogfile:\=/%
-	rem should end up with e.g. wpkglogfile=/cygdrive/c/wpkg-%computername%.log
-	rem and send the wpkglogfile
-	rsync -vh  -e "ssh -i %wpkgidfile% -o UserKnownHostsFile=%wpkg_hosts%" --timeout=120 %bwlimit% --log-file=%wpkglogfile% %wpkglogfile% %wpkgremote%:/opt/wpkgreports/ 2>nul
-	set sync_send=%errorlevel%
-
-)
+:RSYNC-SEND
+rem get last logged off user from eventlog, and append to the logfile
+if exist %SCRIPTS%\wpkguserdetails.vbs cscript //nologo %SCRIPTS%\wpkguserdetails.vbs >> "%wpkglogfile%"
+rem set wpkglogfile for rsync, add /cygdrive/ and remove colons
+set wpkglogfile=/cygdrive/%wpkglogfile::=%
+rem replace \ with /
+set wpkglogfile=%wpkglogfile:\=/%
+rem should end up with e.g. wpkglogfile=/cygdrive/c/wpkg-%computername%.log
+rem and send the wpkglogfile
+rsync -vh  -e "ssh -i %wpkgidfile% -o UserKnownHostsFile=%wpkg_hosts%" --timeout=120 %bwlimit% --log-file=%wpkglogfile% %wpkglogfile% %wpkgremote%:/opt/wpkgreports/ 2>nul
+set sync_send=%errorlevel%
 
 :FIX_ACLS
 echo setting file ownership...
+echo setting file ownership... >> "%temp%\%scriptname%_acl.log"
 rem takeown /File to /Administrators group /Recurse /D prompt Y, send error out and cmd out to the log file
 takeown /F %ProgramData%\wpkg /A /R /D Y > "%temp%\%scriptname%_acl.log" 2>&1
 set takeown=%errorlevel%
 
 echo setting file permissions...
-rem /T change Tree (recurse) /Grant, send error out and cmd out to the log file
+echo setting file permissions... >> "%temp%\%scriptname%_acl.log"
+rem /T change Tree (recurse) /Grant, output to log file, apart from error text to CON which we can't capture
 icacls %ProgramData%\wpkg /T /grant "Administrators":F "SYSTEM":F >> "%temp%\%scriptname%_acl.log" 2>&1
 set icacls=%errorlevel%
 
-rem if there was an error, then append the error log to the end of the logfile (might not work in setup mode)
-if not %takeown%.%icacls%==0.0 (
-	type "%temp%\%scriptname%.acl.log" >> %wpkglogfile%
-	exit /b 103
-)
+rem if there was an error
+if '%takeown%.%icacls%'=='0.0' goto END
+rem append acl log to the end of the logfile (might not work in setup mode)
+type "%temp%\%scriptname%.acl.log" >> %wpkglogfile%
+rem and quit
+exit /b 103
 
-if exist "%temp%\%scriptname%.acl.log" del "%temp%\%scriptname%.acl.log"
 :END
+if exist "%temp%\%scriptname%.acl.log" del "%temp%\%scriptname%.acl.log"
